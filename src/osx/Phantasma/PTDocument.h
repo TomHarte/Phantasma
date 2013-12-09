21b1bfbd4100f233422c6552d31ed6d429372202
Thomas Harte <thomas.harte@gmail.com>
2013-12-08 16:55:38 -0800

    Started reformulating as a document-based project.

---
+++ src/osx/Phantasma/PTDocument.h
@@ -0,0 +1,13 @@
+//
+//  PTDocument.h
+//  Phantasma
+//
+//  Created by Thomas Harte on 08/12/2013.
+//  Copyright (c) 2013 Thomas Harte. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PTDocument : NSDocument
+
+@end
---
+++ src/osx/Phantasma/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Phantasma
+//
+//  Created by Thomas Harte on 08/12/2013.
+//  Copyright (c) 2013 Thomas Harte. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
