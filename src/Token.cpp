7403c5221706cced6f74d225b8c3dc31fa104b67
Thomas Harte <thomas.harte@gmail.com>
2013-12-08 19:24:25 -0800

    Fleshed out implementation.

--- src/Token.cpp
+++ src/Token.cpp
@@ -8,6 +8,28 @@
 
 #include "Token.h"
 
+Token::Token(Type _type)
+{
+	type = _type;
+}
+
+Token::Token(std::string *_string)
+{
+	type = Token::STRINGLITERAL;
+	string = _string;
+}
+
+Token::Token(Type _type, uint32_t _value)
+{
+	type = _type;
+	value = _value;
+}
+
+Token::Type Token::getType()
+{
+	return type;
+}
+
 uint32_t Token::GetValue(CGameState *gameState, uint32_t suggestedValue)
 {
 	switch(type)
@@ -16,4 +38,10 @@ uint32_t Token::GetValue(CGameState *gameState, uint32_t suggestedValue)
 		case VARIABLE:	return gameState->GetVariable(value);
 		default:		return suggestedValue;
 	}
+}
+
+Token::~Token()
+{
+	if(type == Token::STRINGLITERAL && string)
+		delete string;
 }
