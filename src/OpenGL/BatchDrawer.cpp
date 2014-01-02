0d9cd6c4c7f12c94545eccb26e659211bbef9d0c
Thomas Harte <thomas.harte@gmail.com>
2014-01-01 21:04:58 -0800

    Fixed flushing logic.

--- src/OpenGL/BatchDrawer.cpp
+++ src/OpenGL/BatchDrawer.cpp
@@ -18,14 +18,24 @@ BatchDrawer::BatchDrawer()
 
 void BatchDrawer::drawElements(VertexBuffer *vertexBuffer, DrawElementsBuffer *drawElementsBuffer, GLenum mode, GLsizei count, GLenum type, size_t _startIndex, float polygonOffset)
 {
-//	glPolygonOffset(polygonOffset, polygonOffset);
-//
-//	vertexBuffer->bind();
-//	drawElementsBuffer->bind();
-//	glDrawElements(mode, count, type, (void *)_startIndex);
-//	return;
+	// perform an implicit flush if necessary
+	if(
+		hasDrawOngoing &&
+		(
+			(vertexBuffer != currentVertexBuffer) ||
+			(drawElementsBuffer != currentDrawElementsBuffer) ||
+			(currentMode != mode) ||
+			(currentType != type) ||
+			(currentPolygonOffset != polygonOffset) ||
+			(startIndex + ((size_t)currentCount * glptSizeOfType(type)) != _startIndex)
+		)
+	)
+	{
+		flush();
+	}
 
-	// if we don't have a batch started, just go
+	// if we don't have a batch started, start one;
+	// otherwise extend the one we have
 	if(!hasDrawOngoing)
 	{
 		currentVertexBuffer = vertexBuffer;
@@ -36,33 +46,10 @@ void BatchDrawer::drawElements(VertexBuffer *vertexBuffer, DrawElementsBuffer *d
 		startIndex = _startIndex;
 		currentPolygonOffset = polygonOffset;
 		hasDrawOngoing = true;
-
-		return;
-	}
-
-	// if we've got a type incompatibility, draw and return now
-	if(
-		(vertexBuffer != currentVertexBuffer) ||
-		(drawElementsBuffer != currentDrawElementsBuffer) ||
-		(currentMode != mode) ||
-		(currentType != type) ||
-		(currentPolygonOffset != polygonOffset)
-	)
-	{
-		flush();
-		return;
-	}
-
-	// if this is an extension of the current list, add it to the end;
-	// otherwise draw now and return
-	if(startIndex + ((size_t)currentCount * glptSizeOfType(type)) == _startIndex)
-	{
-		currentCount += count;
 	}
 	else
 	{
-		flush();
-		return;
+		currentCount += count;
 	}
 }
 
@@ -70,13 +57,12 @@ void BatchDrawer::flush()
 {
 	if(hasDrawOngoing)
 	{
-		glPolygonOffset(currentPolygonOffset, currentPolygonOffset);
-
 		currentVertexBuffer->bind();
 		currentDrawElementsBuffer->bind();
-		glDrawElements(currentMode, currentCount, currentType, (void *)startIndex);
-		hasDrawOngoing = false;
 
-//		std::cout << ".";
+		glPolygonOffset(currentPolygonOffset, currentPolygonOffset);
+		glDrawElements(currentMode, currentCount, currentType, (void *)startIndex);
+
+		hasDrawOngoing = false;
 	}
 }
